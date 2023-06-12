b34c853ec99fbec5b278e6ec4ece2165230e5cad
akram2032 <89083331+akram2032@users.noreply.github.com>
2023-06-12 13:17:09 +0100

    final version of the final version

--- server/connect.js
+++ server/connect.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "localhost",
     user: "aquarob",
-    password: "aquarob",
+    //password: "aquarob",
     database: "bathyrob",
 });
 
